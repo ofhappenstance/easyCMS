dff95c6b11e1ca04fe3bc01acff473a2baaf5fec
Sandy Yu <throughthegrapevines@gmail.com>
2014-10-12 13:00:07 -0400

    rendering templates and redirecting actions

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,18 @@ class DemoController < ApplicationController
   layout false
   
   def index
+    render('hello')
+  end
+
+  def hello
+    render('index')
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def google
+    redirect_to("http://google.com")
   end
 end
