84a9c4ba63198b2e9d6226fe9064c7b4c0315f98
Sandy Yu <throughthegrapevines@gmail.com>
2014-10-14 17:23:33 -0400

    create database and model User

---
+++ db/migrate/20141014192107_create_users.rb
@@ -0,0 +1,26 @@
+class CreateUsers < ActiveRecord::Migration
+  # def change
+  #   create_table :users do |t|
+
+  #     t.timestamps
+  #   end
+  # end
+
+  def up
+    create_table :users do |t|
+      t.column "first_name", :string, :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => "", :null => false
+      t.string "password", :limit => 40
+
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+
+end
