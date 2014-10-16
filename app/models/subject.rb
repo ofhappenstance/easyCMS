20d28deea4d3605fa9bc8eb6cdcf25731bfd2503
Sandy Yu <throughthegrapevines@gmail.com>
2014-10-16 14:15:18 -0400

    include named scoped, renamed user model to admin_user model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,14 @@
 class Subject < ActiveRecord::Base
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("subjects.position ASC") }
+  scope :newest_first, lambda { order("subjects.created_at DESC") }
+  scope :search, lambda {|query|
+    where(["name LIKE ?", "%#{query}%"])
+  } #the % means wildcard, ow when I call search with a search term, 
+    #so that it will find query even if it exists as a sub-string.
+    #it will look for a subject whose name has that search term somewhere in it.
+
+
 end
