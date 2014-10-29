0b9fccbf37d2b17bd882e9357b828bf91191752f
Sandy Yu <throughthegrapevines@gmail.com>
2014-10-29 15:46:39 -0400

    subjects create action

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -16,6 +16,19 @@ class SubjectsController < ApplicationController
     @subject = Subject.new({:name => "First Name Last Name"})
   end
 
+  def create 
+    # instantiate a new object using form parameter 
+    @subject = Subject.new(params[:subject])
+    # save the object 
+    if @subject.save
+    # if save succeeds, redirect to the index action 
+      redirect_to(:action => 'index')
+    else
+    # if save fails, redisplay the form so user can fix problems 
+      render('new')
+    end
+  end
+
   def edit
   end
 
