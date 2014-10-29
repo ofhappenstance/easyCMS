8a6f764554bb53b7e2340d1888a3f8cc163bd4a4
Sandy Yu <throughthegrapevines@gmail.com>
2014-10-29 17:15:20 -0400

    add edit and update actions to Subjects, add link to edit on subject index and add edit template

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -30,6 +30,21 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    # Find an existing object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If update succeeds, redirect to the index action 
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      # If update fails, redisplay the form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
@@ -43,5 +58,5 @@ class SubjectsController < ApplicationController
       # - allows listed attributes to be mass-assigned
       params.require(:subject).permit(:name, :position, :visible)
     end
-    
+
 end
