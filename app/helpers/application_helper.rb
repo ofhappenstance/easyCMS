454aaa47c3700d9da6b0624fb6ef2584df9445e0
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-01 12:03:41 -0400

    moving code to application_helper, adding helper to pages and section view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
-module ApplicationHelper
+module ApplicationHelper   
+  # be able to call status_tag and pass it a boolean as well as any options defined below
+  def status_tag(boolean, options={})
+    options[:true_text] ||= ''
+    options[:false_text] ||=''
+
+    if boolean
+      # generate new span tag with helper content_tag (an html helper)
+      # span tag will get a class status true and insert in text 
+      content_tag(:span, options[:true_text], :class => 'status true')
+    else
+      content_tag(:span, options[:false_text], :class => 'status false')
+    end
+  end
 end
--- app/helpers/subjects_helper.rb
+++ app/helpers/subjects_helper.rb
@@ -1,17 +1,4 @@
 module SubjectsHelper
-  
-  # be bale to call status_tag and pass it a boolean as well as any options defined below
-  def status_tag(boolean, options={})
-    options[:true_text] ||= ''
-    options[:false_text] ||=''
 
-    if boolean
-      # generate new span tag with helper content_tag (an html helper)
-      # span tag will get a class status true and insert in text 
-      content_tag(:span, options[:true_text], :class => 'status true')
-    else
-      content_tag(:span, options[:false_text], :class => 'status false')
-    end
-  end
 
 end
