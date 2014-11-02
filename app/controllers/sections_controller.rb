e5e3186a1429d62cc75f8eff2f31428d00d15cd3
Sandy Yu <throughthegrapevines@gmail.com>
2014-11-02 12:49:55 -0500

    nesting sections to page

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,11 +1,12 @@
 class SectionsController < ApplicationController
-  
+
   layout "admin"
-  
+
   before_action :confirm_logged_in
+  before_action :find_page
 
   def index
-    @sections = Section.sorted
+    @sections = @page.sections.sorted
   end
 
   def show
@@ -13,37 +14,37 @@ class SectionsController < ApplicationController
   end
 
   def new
-    @section = Section.new({:name => "Default"})
+    @section = Section.new({:page_id => @page.id, :name => "Default"})
+    @pages = @page.subject.pages.sorted
     @section_count = Section.count + 1
-    @pages = Page.order('position ASC')
   end
 
   def create
     @section = Section.new(section_params)
     if @section.save
-      flash[:notice] = "Section created successfully"
-      redirect_to(:action => 'index')
+      flash[:notice] = "Section created successfully."
+      redirect_to(:action => 'index', :page_id => @page.id)
     else
+      @pages = @page.subject.pages.sorted
       @section_count = Section.count + 1
-      @pages = Page.order('position ASC')
       render('new')
     end
   end
 
   def edit
     @section = Section.find(params[:id])
+    @pages = @page.subject.pages.sorted
     @section_count = Section.count
-    @pages = Page.order('position ASC')
   end
 
   def update
     @section = Section.find(params[:id])
     if @section.update_attributes(section_params)
-      flash[:notice] = "Section updated successfully"
-      redirect_to(:action => 'show', :id => @section.id)
+      flash[:notice] = "Section updated successfully."
+      redirect_to(:action => 'show', :id => @section.id, :page_id => @page.id)
     else
+      @pages = @page.subject.pages.sorted
       @section_count = Section.count
-      @pages = Page.order('position ASC')
       render('edit')
     end
   end
@@ -54,14 +55,20 @@ class SectionsController < ApplicationController
 
   def destroy
     section = Section.find(params[:id]).destroy
-    flash[:notice] = "Section destroyed successfully"
-    redirect_to(:action => 'index')
+    flash[:notice] = "Section destroyed successfully."
+    redirect_to(:action => 'index', :page_id => @page.id)
   end
+
 
   private
 
-  def section_params
-    params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
-  end
+    def section_params
+      params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
+    end
 
+    def find_page
+      if params[:page_id]
+        @page = Page.find(params[:page_id])
+      end
+    end
 end
